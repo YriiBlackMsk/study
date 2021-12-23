db14e61912cb1ae36a09c5c7471e35945a8b19e5
BLACK <yriiblack@g,ail.com>
2021-12-23 13:23:15 +0300

    добавлена стилизация

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const borderStyle = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(36)),
+      borderSide: BorderSide(
+        color: const Color(0xFFbbbbbb), width: 2));
+     const LinkTextStyle = TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF0079D0)
+    );
     return MaterialApp(
       home: Scaffold(
           body: Container(
@@ -18,13 +27,16 @@ class MyApp extends StatelessWidget {
               SizedBox(height: 60,),
               SizedBox(width: 110, height: 84, child: Placeholder(),),
               SizedBox(height: 20,),
-              Text('Введите логин в виде 10 цифр номера телефона'),
+              Text('Введите логин в виде 10 цифр номера телефона', 
+                style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6) ),),
               SizedBox(height: 20,),
               const SizedBox(width: 244,
                 child: TextField(
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Color(0xFFeceff1),
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
                     labelText: 'Телефон',
                   ),
                 ),
@@ -36,16 +48,30 @@ class MyApp extends StatelessWidget {
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Color(0xFFeceff1),
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
                     labelText: 'Пароль',
                   ),
                 ),
               ),
               SizedBox(height: 20,),
-              SizedBox(width: 154, height: 42, child: ElevatedButton(onPressed: () {}, child: Text('Войти'))),
-              SizedBox(height: 62,),
-              InkWell(child: Text('Регистрация'), onTap: () {}),
+              SizedBox(width: 154, height: 42, child:
+               ElevatedButton(onPressed: () {},
+                  child: Text('Войти'),
+               style: ElevatedButton.styleFrom(
+                   primary: Color(0xFF0079D0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(36.0),
+                 ),
+                ),
+               )
+              ),
+              SizedBox(height: 32,),
+              InkWell(child: Text('Регистрация',
+               style: LinkTextStyle,), onTap: () {}),
               SizedBox(height: 20,),
-              InkWell(child: Text('Забыли пароль?'), onTap: () {}),
+              InkWell(child: Text('Забыли пароль?',
+                style: LinkTextStyle,), onTap: () {}),
             ],),
           )
       ),
